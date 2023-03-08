2337ceee6a77e13aac494fd4a59ce02eaed7e473
Dhruv <dpahuja46@gmail.com>
2023-03-08 10:49:36 +0530

    added component testing

---
+++ packages/my-app/src/app/app.cy.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import App from './app'
+
+
+describe(App.name, () => {
+  it('renders', () => {
+    cy.mount(<App />)
+  })
+})
+
---
+++ packages/my-app/src/app/nx-welcome.cy.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import NxWelcome from './nx-welcome'
+
+
+describe(NxWelcome.name, () => {
+  it('renders', () => {
+    cy.mount(<NxWelcome />)
+  })
+})
+
